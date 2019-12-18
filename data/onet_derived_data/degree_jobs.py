f1627f67d51b4ff9a328fcb1ee595369c74cfdba
rudyarthur <rudy.d.arthur@gmail.com>
2019-12-18 12:15:14 +0000

    network

--- data/onet_derived_data/degree_jobs.py
+++ data/onet_derived_data/degree_jobs.py
@@ -1,32 +1,14 @@
-import pandas as pd
 import os
 import glob
 import csv
 import sys
 from collections import Counter
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.cluster import SpectralClustering
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_samples, silhouette_score
+import scipy.stats as stats
 
-from scipy import stats
+from get_all_jobs import *
 
-
-code_to_job = {}
-job_to_code = {}
-for fname in ["../db_24_0_text/Occupation Data.txt"]:
-	with open(fname, 'r') as csvfile:
-		csvreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
-		header = next(csvreader);
-		hmap = { h:i for i,h in enumerate(header) }
-		
-		for row in csvreader:
-			code_to_job[ row[0] ] = row[1]
+code_to_job = get_code_to_job();
 
 code_to_dist = {}	
 for fname in ["../db_24_0_text/Education, Training, and Experience.txt"]:
@@ -44,8 +26,6 @@ for fname in ["../db_24_0_text/Education, Training, and Experience.txt"]:
 					code_to_dist[ job ] = {}
 				code_to_dist[ job ][ int(row[ hmap['Category'] ]) ] = float( row[ hmap['Data Value'] ] )
 
-
-threshold=50
 use_jobs = {}
 imps = []
 for c in code_to_dist:
@@ -53,42 +33,22 @@ for c in code_to_dist:
 	use_jobs[c] = degree_importance
 	imps.append(degree_importance)
 	
-with open("degree_jobs.txt".format(threshold), 'w') as outfile:
+with open("degree_jobs.txt", 'w') as outfile:
 	outfile.write("O*NET-SOC Code\tTitle\tImportance\n")
 	for c in sorted( use_jobs, key=use_jobs.get, reverse=True ):
 		outfile.write("{}\t{}\t{}\n".format(c, code_to_job[c], use_jobs[c]))
 	
 
-
-attribute_to_vector = {}
-attributes = set();
-jobs = set();
-for fname in ["../db_24_0_text/Work Styles.txt", "../db_24_0_text/Work Context.txt", "../db_24_0_text/Work Activities.txt", "../db_24_0_text/Knowledge.txt", "../db_24_0_text/Skills.txt","../db_24_0_text/Abilities.txt" ]:
-	with open(fname, 'r') as csvfile:
-		csvreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
-		header = next(csvreader);
-		hmap = { h:i for i,h in enumerate(header) }
-
-		for row in csvreader:
-			attribute = row[ hmap['Element Name'] ];
-			job = row[ hmap['O*NET-SOC Code'] ];
-			if job == "15-2091.00": continue
-			jobs.add(job)
-			attributes.add(attribute);
-
-			if row[ hmap['Scale ID'] ] == "IM" or row[ hmap['Scale ID'] ] == "CX":
-				if attribute not in attribute_to_vector:
-					attribute_to_vector[ attribute ] = {}
-				attribute_to_vector[ attribute ][ job ] = float( row[ hmap['Data Value'] ] )
-
+jobs, attributes, attribute_to_vector = get_all_jobs();
 jobs = sorted(jobs)
+
 degree_importance = [ use_jobs[c] for c in jobs ]
 attrib_to_degree = {}
 for a in attribute_to_vector:
 	skill_importance = [ attribute_to_vector[ a ][ c ] for c in jobs  ]
 	attrib_to_degree[a] = stats.pearsonr(skill_importance, degree_importance)[0]
 
-with open("degree_skills.txt".format(threshold), 'w') as outfile:
+with open("degree_attributes.txt", 'w') as outfile:
 	outfile.write("Attribute\tCorrelation\n")
 	for c in sorted( attrib_to_degree, key=attrib_to_degree.get, reverse=True ):
 		outfile.write("{}\t{}\n".format(c, attrib_to_degree[c]))
---
+++ data/onet_derived_data/network_rca.py
@@ -0,0 +1,94 @@
+import os
+import glob
+import csv
+import sys
+from collections import Counter
+import numpy as np
+import scipy.stats as stats
+import networkx as nx
+from networkx.algorithms import bipartite
+import matplotlib.pyplot as plt
+from get_all_jobs import *
+import math
+
+	
+code_to_job = get_code_to_job();
+jobs, attributes, attribute_to_vector = get_all_jobs();
+jobs = sorted(jobs)
+attributes = sorted(attributes)
+
+job_threshold = 50;
+attribute_threshold = 0;
+edge_threshold = 0.7
+
+degree_jobs = set();
+with open("degree_jobs.txt", 'r') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+	header = next(csvreader);
+	for row in csvreader:
+		if float(row[2]) >= job_threshold:
+			degree_jobs.add( row[0] );
+
+degree_attributes = set();
+with open("degree_attributes.txt", 'r') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+	header = next(csvreader);
+	for row in csvreader:
+		if float(row[1]) > attribute_threshold:
+			degree_attributes.add( row[0] );
+
+print(len(degree_jobs), len(degree_attributes))
+
+norm = 0
+for j in degree_jobs:
+	for s in degree_attributes:
+		norm += attribute_to_vector[s][j]
+
+jvec = Counter()
+for j in degree_jobs:
+	for s in degree_attributes:
+		jvec[j] += attribute_to_vector[s][j]
+	
+svec = Counter();			
+for s in degree_attributes:
+	for j in degree_jobs:
+		svec[s] += attribute_to_vector[s][j]
+
+rca = {}	
+B = nx.Graph()	
+for j in degree_jobs:
+	rca[j] = {}
+	for s in degree_attributes:			
+		rca[j][s] = (attribute_to_vector[s][j]/jvec[j])/(svec[s]/norm);
+		if rca[j][s] > 1:
+			B.add_node(j, bipartite=0)
+			B.add_node(s, bipartite=1)
+			B.add_edge(j, s, weight=1)
+
+G  = bipartite.weighted_projected_graph(B, [j for j in jobs if B.has_node(j)] )
+print(len(G.nodes()), len(G.edges()))
+#ews = []
+for u, v in G.edges():
+	G[u][v]["weight"] = G[u][v]["weight"] / max( B.degree[u], B.degree[v] )
+#	ews.append( G[u][v]["weight"])
+#plt.hist(ews, bins=100, density=True, alpha=0.5, cumulative=True)
+#plt.savefig("attribute_ews.png");
+#plt.show();
+#plt.close();	
+
+H = nx.Graph();
+elist = []
+nlist = set();
+for u, v in G.edges():
+	if G[u][v]["weight"]  > edge_threshold:
+		H.add_edge(u, v, weight=G[u][v]["weight"])
+		elist.append( (u,v) )
+		nlist.add(u)
+		nlist.add(v)
+		
+pos = nx.spring_layout(H, seed=123456); #, k=16/math.sqrt(H.size()), seed=123)
+nx.draw_networkx_nodes(H, pos=pos, node_size=30, nodelist=list(nlist))
+nx.draw_networkx_edges(H, pos=pos, edgelist=elist)
+plt.savefig("DegreeJobNetwork.png")
+
+
